510490da5d93ad448582136d4d6adca159b230d6
Govi <govi18051969@gmail.com>
2023-02-15 14:51:57 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/UrlShortener", {
+      useNewUrlParser: true
+  })
+  .then((d) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+module.exports = mongoose
