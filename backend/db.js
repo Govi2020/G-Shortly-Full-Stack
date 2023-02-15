3d714701d8a5405a89d5168a2ca190a41e63ff2f
Govi <govi18051969@gmail.com>
2023-02-15 16:06:11 +0530

    Added Mongo Altas

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/UrlShortener", {
+  .connect(process.env.MONGO_DB_URL || "mongodb://127.0.0.1:27017/UrlShortener", {
       useNewUrlParser: true
   })
   .then((d) => {
